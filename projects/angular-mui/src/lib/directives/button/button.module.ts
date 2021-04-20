18c58413285dbcf99d5040e6481a790937798d46
rachitgupta98 <rachit371@gmail.com>
2021-04-20 13:02:41 +0530

    add mui-button

---
+++ projects/angular-mui/src/lib/directives/button/button.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonDirective } from './button.directive';
+
+
+
+@NgModule({
+  declarations: [ButtonDirective],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonModule { }
